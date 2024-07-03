f79859207bfdcf75da9b81b875c74a6855fe98fd
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-07-03 10:16:19 -0300

    feat: adiciona card para mostrar ptodutos

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { Product } from '../../interfaces/product';
+import {MatCardModule} from '@angular/material/card'
+import {MatButtonModule} from '@angular/material/button'
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
