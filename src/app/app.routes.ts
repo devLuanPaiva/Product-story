b75412d87a3e2117df43dd8bae86c25f223c9b76
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-07-04 16:18:18 -0300

    feat: adiciona pagina de edição

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,19 @@ import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
 
 export const routes: Routes = [
-    { path: '', component: ListComponent },
-    { path: 'create-product', loadComponent: () =>
-        import('./features/create/create.component').then(m => m.CreateComponent) },
+  { path: '', component: ListComponent },
+  {
+    path: 'create-product',
+    loadComponent: () =>
+      import('./features/create/create.component').then(
+        (m) => m.CreateComponent
+      ),
+  },
+  {
+    path: 'edit-product',
+    loadComponent: () =>
+      import('./features/edit/edit.component').then(
+        (m) => m.EditComponent
+      ),
+  },
 ];
