9a4d6d5e85cd00118170af9c1f822005de41eaec
Luan Alves <97049835+Luan029@users.noreply.github.com>
2024-07-08 15:48:33 -0300

    feat: cria componente para ser exibido quando não há produtos.

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -6,16 +6,23 @@ import { Router, RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { ConfirmationDialogService } from '../../services/confirmation-dialog.service';
 import { filter } from 'rxjs';
+import { NoItemsComponent } from './components/no-items/no-items.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent, RouterLink, MatButtonModule],
+  imports: [CardComponent, RouterLink, MatButtonModule, NoItemsComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
 export class ListComponent {
+<<<<<<< HEAD
   products: Product[] = [];
+=======
+  products = signal<Product[]>(
+    inject(ActivatedRoute).snapshot.data['products']
+  );
+>>>>>>> 8327a43 (feat: cria componente para ser exibido quando não há produtos)
   productsService = inject(ProductsService);
   router = inject(Router);
   confirmationDialogService = inject(ConfirmationDialogService);
@@ -30,14 +37,23 @@ export class ListComponent {
   }
   onDelete(product: Product) {
     this.confirmationDialogService
+<<<<<<< HEAD
     .openDialog()
     .pipe(filter((answer) => answer === true))
     .subscribe(() => {
       this.productsService.delete(product.id).subscribe(() => {
         this.productsService.getAll().subscribe((products) => {
           this.products = products;
+=======
+      .openDialog()
+      .pipe(filter((answer) => answer === true))
+      .subscribe(() => {
+        this.productsService.delete(product.id).subscribe(() => {
+          this.productsService.getAll().subscribe((products) => {
+            this.products.set(products);
+          });
+>>>>>>> 8327a43 (feat: cria componente para ser exibido quando não há produtos)
         });
       });
-    });
   }
 }
